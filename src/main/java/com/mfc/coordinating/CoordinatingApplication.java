ef8a80ef14495e33ca78a2dfaad22f0e269a97bf
Moriah(모리) <49335804+KwanHoo@users.noreply.github.com>
2024-05-16 16:25:15 +0900

    feat : init project

---
+++ src/main/java/com/mfc/coordinating/CoordinatingApplication.java
@@ -0,0 +1,13 @@
+package com.mfc.coordinating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoordinatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoordinatingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mfc/coordinating/CoordinatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfc.coordinating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoordinatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
