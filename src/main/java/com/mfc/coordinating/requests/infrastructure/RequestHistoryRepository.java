8b043ae0a9be12f970f4c4ade606c6468431ab25
pockyjx <0127828b@gmail.com>
2024-06-08 16:46:15 +0900

    fix: jpa-mongodb repository 설정 충돌 문제 수정

--- src/main/java/com/mfc/coordinating/requests/infrastructure/RequestHistoryRepository.java
+++ src/main/java/com/mfc/coordinating/requests/infrastructure/RequestHistoryRepository.java
@@ -2,11 +2,11 @@ package com.mfc.coordinating.requests.infrastructure;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.mfc.coordinating.requests.domain.RequestHistory;
 
-public interface RequestHistoryRepository extends JpaRepository<RequestHistory, Long> {
+public interface RequestHistoryRepository extends MongoRepository<RequestHistory, Long> {
 
 	Page<RequestHistory> findByPartnerId(String partnerId, Pageable pageable);
 
